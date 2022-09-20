64b34a7ed57a317657006a7d47584db766a5941b
chaesanamidaarumdapta <113958419+chaesanamidaarumdapta@users.noreply.github.com>
2022-09-20 11:19:51 +0700

    Add files via upload

---
+++ latihan 1 algoritma.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=input("nama kamu siapa ?")
+y=input("dari mana asal kamu chaesa?")
+z=input("sebelumnya bersekolah dimana ?")
+
+print("hallo selamat siang " +x)
+print("wah dari " +y)
+print("pasti menyenangkan bersekolah " +z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
